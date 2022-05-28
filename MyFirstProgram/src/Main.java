f36d9271657d8fd3a18c900e48c0abb7e2ab9e56
RmdD999 <Raymondu1996@outlook.com>
2022-05-28 23:27:25 +0800

    For some slash using

--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -3,8 +3,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("I love Pizza!");
-		System.out.println("It's really good!");
+		System.out.println("I love Pizza!\n");
+		System.out.println("\tIt's really good!");
+		System.out.println("\"Hello World!\"");
+		System.out.println("\\Hello World! Again?\\");
+		
+		//This is a comment
+		/*
+		 * This
+		 * Is
+		 * A
+		 * COMMENT
+		 */
 	}
 	
 
