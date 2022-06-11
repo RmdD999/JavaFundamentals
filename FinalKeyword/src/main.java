e1e33a520676b7f416be0396a1a4b4ddeb555e5a
RmdD999 <Raymondu1996@outlook.com>
2022-06-12 00:57:13 +0800

    Final keyword

---
+++ FinalKeyword/src/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		final double pi = 3.1415926; // you can't change it when declare with a final keyword.
+		
+		
+		
+		System.out.println(pi);
+		
+	}
+
+}
