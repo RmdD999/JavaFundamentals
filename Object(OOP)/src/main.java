f483b67f9e77b7fbdd6af918b08699ecfd83b02c
RmdD999 <Raymondu1996@outlook.com>
2022-06-12 01:12:21 +0800

    Create a new Car class

---
+++ Object(OOP)/src/main.java
@@ -0,0 +1,21 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		//object = an instance of a class that may contain attributes and methods
+		//example: (phone, desk, computer, coffee cup)
+		
+		Car myCar = new Car();
+		
+		System.out.println(myCar.make);
+		System.out.println(myCar.model);
+		System.out.println(myCar.color);
+		System.out.println(myCar.year);
+		System.out.println(myCar.price);
+
+		myCar.drive();
+		myCar.brake();
+	}
+
+}
