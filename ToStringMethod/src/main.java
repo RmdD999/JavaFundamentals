4bf695d1f0217a2c7b6b00a52da5fee6467a5aed
RmdD999 <Raymondu1996@outlook.com>
2022-06-12 15:07:29 +0800

    ToString method

---
+++ ToStringMethod/src/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		//toString() = special method that all objects inherit.
+		//             that returns a string that "textually represents" an object
+		//             can be used both implicitly and explicitly
+		
+		Car car = new Car();
+		
+		System.out.println(car.toString()); //Car@5a07e868 address in memory.
+
+	}
+
+}
