cfcae90850201f0ff2da3a5108fa2a98a78ff52e
RmdD999 <Raymondu1996@outlook.com>
2022-06-15 09:53:07 +0800

    copy a object

--- Encapsulation/src/main.java
+++ Encapsulation/src/main.java
@@ -8,14 +8,28 @@ public class main {
 		//                  You should make attributes private if you don't have a reason to make them public/protected
 		
 		
-		Car car = new Car("Chevrolet","Camaro",2021);
-		Car car2 = new Car("Benz","g500",2022);
+		Car car1 = new Car("Chevrolet","Camaro",2021);
+//		Car car2 = new Car("Benz","g500",2022);
 		
-	
+		//car2 = car1; //same address/memory now
+		//car2.copy(car1); //same address but with differenct attributes
+		Car car2 =  new Car(car1);
 		
-		car.setMake("Tesla");
-		System.out.println(car.getMake());
+		
+		System.out.println(car1);
+		System.out.println(car2);
+		System.out.println();
+		
+		System.out.println(car1.getMake());
+		System.out.println(car1.getModel());
+		System.out.println(car1.getYear());
+		
+		System.out.println();
+		
 		System.out.println(car2.getMake());
+		System.out.println(car2.getModel());
+		System.out.println(car2.getYear());
+		
 
 	}
 
