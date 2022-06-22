d1dd45fb944b7909d3691976ffc6bcda4da6ce21
Raymondu1996@outlook.com <Raymondu1996@outlook.com>
2022-06-22 15:28:46 +0800

    Combo Box --some useful functions

--- ComboBox/src/Myframe.java
+++ ComboBox/src/Myframe.java
@@ -9,9 +9,18 @@ public class Myframe extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
 
+        Integer[] numbers = {1,3,4}; //use wrapper class for Intergers
         String[] animals ={"Dog","Cat","Tiger"};
         comboBox = new JComboBox(animals);
         comboBox.addActionListener(this);
+        comboBox.setEditable(true); // could edit on the options
+        System.out.println(comboBox.getItemCount());
+        comboBox.addItem("Horse");
+        comboBox.removeItem("Cat");
+        comboBox.removeItemAt(0);
+        //comboBox.removeAllItems(); //remove all items
+        comboBox.insertItemAt("Pig",0); //add at the beginning
+        comboBox.setSelectedIndex(0); //first runs select index as 0
 
         this.add(comboBox);
         this.pack();
@@ -23,6 +32,7 @@ public class Myframe extends JFrame implements ActionListener {
 
         if(e.getSource() == comboBox){
             System.out.println(comboBox.getSelectedItem());
+            System.out.println(comboBox.getSelectedIndex());
         }
     }
 }
