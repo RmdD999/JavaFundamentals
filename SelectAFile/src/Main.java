10c8e1160154fbde79d8b2be8410660b6dfcb9a9
Raymondu1996@outlook.com <Raymondu1996@outlook.com>
2022-06-24 10:40:02 +0800

    JFileChooser

---
+++ SelectAFile/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // JFileChooser = A GUI mechanism that let's user choose a file (helpful for opening or saving files)
+        new MyFrame();
+    }
+}
